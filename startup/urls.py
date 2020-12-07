224fbd4712e59d2358a7f61653c810f5b6abad4c
ZHU Jian <50431146+zhujiansamuel@users.noreply.github.com>
2020-12-07 20:40:19 +0900

    language

--- startup/urls.py
+++ startup/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+
+urlpatterns += i18n_patterns(
     path('', include("mysite.urls")),
     path('validate/', include('validation.urls')),
     path('blog/', include('blog.urls')),
     path('student/', include('student.urls')),
     path('onlinelearning/', include('onlinelearning.urls'))
-]
+)
+
